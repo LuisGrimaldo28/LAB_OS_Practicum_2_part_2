b326bab7941011287ef736e5e0b6fe6fc24eacc5
LuisGrimaldo28 <142188209+LuisGrimaldo28@users.noreply.github.com>
2023-08-13 20:19:39 -0500

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# example of using arguments to a script echo "My first name is $1" 
+echo "My surname is $2" 
+echo "Total number of arguments is $#" 
+#!/bin/bash 
+x=10 
+#NOT× = 10 no spaces 
+X=20 
+#variables are case sensitive 
+y= 
+#NULL variable 
+echo "x = $x" 
+echo "X = $X" 
+echo "y = $y" 
